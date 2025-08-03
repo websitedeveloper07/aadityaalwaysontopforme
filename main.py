30317184eb5c27ca8a31d61e004e5da2b55b78ac
websitedeveloper07 <aadityamoga2@gmail.com>
2025-08-04 00:51:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -259,7 +259,7 @@ async def check_authorization(update: Update, context: ContextTypes.DEFAULT_TYPE
             reply_markup = InlineKeyboardMarkup(keyboard)
             await update.effective_message.reply_text(
                 "🚫 *Private Usage Blocked*\nYou cannot use this bot in private chat.\n\nUse /plans to upgrade or join our group to access tools for free.",
-                "Get a subscription  from `@enough69s` to use this bot, "
+                "Get a subscription  from `@enough69s` to use this bot. ",
                 reply_markup=reply_markup,
                 parse_mode=ParseMode.MARKDOWN_V2
             )
