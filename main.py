1e54972cf30e684c3b31ec727d2cf11786183e0d
websitedeveloper07 <aadityamoga2@gmail.com>
2025-08-10 19:33:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -787,7 +787,7 @@ async def chk_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         f"✘ Country    ➜ {escape_markdown(country_name, version=2)}\n"
         "――――――――――――――――\n"
         f"✘ Request By  ➜ {escape_markdown(user.first_name, version=2)}\\[{escape_markdown(user_data.get('plan','Free'), version=2)}\\]\n"
-        "✘ Developer   ➜ Darkboy X7\n"
+        "✘ Developer   ➜ @K4linuxx\n"
         f"✘ Time        ➜ {escape_markdown(str(time_taken), version=2)} seconds\n"
         "――――――――――――――――"
     )
