b2d5c7460f5efe2e5f099f3ab2ffc854cbfc6745
websitedeveloper07 <aadityamoga2@gmail.com>
2025-08-10 14:32:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ OWNER_ID = 7796598050  # Replace with your Telegram user ID
 OFFICIAL_GROUP_LINK = "https://t.me/CARDER33"  # Replace with your group link
 AUTHORIZED_PRIVATE_USERS = set()  # Start empty, or preload user IDs
 AUTHORIZED_CHATS = set()  # Start empty, or preload chat IDs
-ADMIN_IDS = [7796598050]  # Replace with your real Telegram user ID
+ADMIN_IDS = [8438505794]  # Replace with your real Telegram user ID
