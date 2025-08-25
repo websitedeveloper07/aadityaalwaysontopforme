8326b93998bf96188a75fa658f945e3774710251
websitedeveloper07 <aadityamoga2@gmail.com>
2025-08-25 16:49:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2856,21 +2856,23 @@ async def seturl(update: Update, context: ContextTypes.DEFAULT_TYPE):
         developer_clickable = f"<a href='{DEVELOPER_LINK}'>{DEVELOPER_NAME}</a>"
 
         BULLET_GROUP_LINK = "https://t.me/+pu4_ZBdp1CxiMDE1"
-        bullet_text = escape_all_markdown("[⌇]")
-        bullet_link = f"[{bullet_text}]({BULLET_GROUP_LINK})"
+        bullet_text = "[⌇]"
+        bullet_link = f'<a href="{BULLET_GROUP_LINK}">{bullet_text}</a>'
 
         site_status = "✅ 𝐒𝐢𝐭𝐞 𝐀𝐝𝐝𝐞𝐝" if "Error" not in response else "❌ 𝐅𝐚𝐢𝐥𝐞𝐝"
 
-        formatted_msg = (
-            f"═══[ <b>{site_status}</b> ]═══\n"
-            f"{bullet_link} <b>𝐒𝐢𝐭𝐞</b> ➜ <code>{escape(site_input)}</code>\n"
-            f"{bullet_link} <b>𝐀𝐦𝐨𝐮𝐧𝐭</b> ➜ {escape(price)}💸\n"
-            f"{bullet_link} <b>𝐑𝐞𝐬𝐩𝐨𝐧𝐬𝐞</b> ➜ <i>{escape(response)}</i>\n"
-            f"――――――――――――――――\n"
-            f"{bullet_link} <b>𝐑𝐞𝐪𝐮𝐞𝐬𝐭𝐞𝐝 𝐁𝐲</b> ➜ {requester}\n"
-            f"{bullet_link} <b>𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫</b> ➜ {developer_clickable}\n"
-            f"――――――――――――――――"
+       formatted_msg = (
+        f"═══[ <b>{site_status}</b> ]═══\n"
+        f"{bullet_link} <b>𝐒𝐢𝐭𝐞</b> ➜ <code>{escape(site_input)}</code>\n"
+        f"{bullet_link} <b>𝐀𝐦𝐨𝐮𝐧𝐭</b> ➜ {escape(price)}💸\n"
+        f"{bullet_link} <b>𝐑𝐞𝐬𝐩𝐨𝐧𝐬𝐞</b> ➜ <i>{escape(response)}</i>\n"
+        "――――――――――――――――\n"
+        f"{bullet_link} <b>𝐑𝐞𝐪𝐮𝐞𝐬𝐭𝐞𝐝 𝐁𝐲</b> ➜ {requester}\n"
+        f"{bullet_link} <b>𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫</b> ➜ {developer_clickable}\n"
+        "――――――――――――――――"
         )
+
+
 
         await processing_msg.edit_text(
             formatted_msg,
