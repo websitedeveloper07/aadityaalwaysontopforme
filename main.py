04cf5dc7020d2978296f6d3c546ecf5f1f16f263
websitedeveloper07 <aadityamoga2@gmail.com>
2025-09-07 23:47:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4289,7 +4289,6 @@ def register_force_join(application):
     application.add_handler(CommandHandler("chk", force_join(chk_command)))
     application.add_handler(CommandHandler("st", force_join(st)))
     application.add_handler(CommandHandler("mass", force_join(mass_handler)))
-    application.add_handler(CommandHandler("mtchk", force_join(mtchk)))
     application.add_handler(CommandHandler("sh", force_join(sh_command)))
     application.add_handler(CommandHandler("seturl", force_join(seturl)))
     application.add_handler(CommandHandler("mysites", force_join(mysites)))
