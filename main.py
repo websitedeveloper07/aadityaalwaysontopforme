2a000c040b1a8fc5d49352c4ba7e2dcfb484b6ba
websitedeveloper07 <aadityamoga2@gmail.com>
2025-09-18 12:11:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2691,7 +2691,7 @@ async def process_sh(update: Update, context: ContextTypes.DEFAULT_TYPE, payload
         # --- API request ---
         api_url = (
             f"https://auto-shopify-6cz4.onrender.com/index.php"
-            f"?site=https://deansafe.com"
+            f"?site=https://fallinginsand.com"
             f"&cc={full_card}"
             f"&proxy=qhlpirsk-238:96zjmb7awmom@p.webshare.io:80"
         )
